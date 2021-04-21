c1b32b39f7000c6f658e22216d305cf6c76ce309
Michael Hodges <hodges.m@husky.neu.edu>
2021-04-21 14:08:44 -0400

    adding requirements and download link

---
+++ download.sh
@@ -0,0 +1,17 @@
+mkdir ShapeNet
+cd ShapeNet
+curl https://shapenet.cs.stanford.edu/iccv17/partseg/train_data.zip
+unzip -q train_data.zip
+rm train_data.zip
+curl https://shapenet.cs.stanford.edu/iccv17/partseg/train_label.zip
+unzip -q train_label.zip
+rm train_label.zip
+curl https://shapenet.cs.stanford.edu/iccv17/partseg/val_data.zip
+unzip -q val_data.zip
+rm val_data.zip
+curl https://shapenet.cs.stanford.edu/iccv17/partseg/val_label.zip
+unzip -q val_label.zip
+rm val_label.zip
+curl https://shapenet.cs.stanford.edu/iccv17/partseg/test_data.zip
+unzip -q train_data.zip
+rm train_data.zip
